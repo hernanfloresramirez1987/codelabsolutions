1d71624a9b5fdf95f5ad6290ad833d3f413b0149
hernanfloresramirez1987 <hernan.flores@shipedge.com>
2025-04-16 01:57:13 -0400

    add template

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,41 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+
+
+import { /*HttpClient,*/ provideHttpClient } from '@angular/common/http';
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 
 import Aura from '@primeng/themes/aura';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: Aura
-      }
-    }),
+    importProvidersFrom(
+      CommonModule, 
+      BrowserAnimationsModule, 
+    ),
+    provideHttpClient(),
+    providePrimeNG({ /* options */ }),
+    // providePrimeNG({
+    //   theme: {
+    //     preset: Aura
+    //   }
+    // }),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)]
+    provideRouter(routes),
+    
+    MessageService,
+    ConfirmationService,
+    provideRouter(routes),
+    provideAnimations(),
+    provideAnimationsAsync()
+  ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Documentation } from './pages/documentation/documentation';
+import { Crud } from './pages/crud/crud';
+import { Empty } from './pages/empty/empty';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'documentation', component: Documentation },
+    { path: 'crud', component: Crud },
+    { path: 'empty', component: Empty },
+    { path: '**', redirectTo: '/notfound' }
+];
