3fa9e845b429516aefdef20a78ec57f685e69812
Shipedge <hernan.flores@shipedge.com>
2025-04-16 00:03:17 -0400

    Error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ErrorpageComponent } from './errorpage/errorpage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: ErrorpageComponent
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
